cb43f9e50d72d91ecaab9a1df4745ad861615024
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-09-08 18:14:55 +0530

    Task completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-
 import React from "react";
 import './../styles/App.css';
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
+import { Redirect, Switch, Route } from "react-router-dom"; 
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        
+
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/items" />
+          </Route>
+          <Route exact path='/items' component={ItemList}/>
+          <Route path='/items/:id' component={ItemDetail}/>
+        </Switch>
     </div>
   )
 }
