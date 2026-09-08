2250fb776124aabf0c004f7f51618b7b50d9016c
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-09-08 18:18:26 +0530

    Task completed 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,18 @@
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 import ItemList from "./ItemList";
 import ItemDetail from "./ItemDetail";
-import { Redirect, Switch, Route } from "react-router-dom"; 
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-        
-
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/items" />
-          </Route>
-          <Route exact path='/items' component={ItemList}/>
-          <Route path='/items/:id' component={ItemDetail}/>
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={ItemList} />
+        <Route path="/items/:id" component={ItemDetail} />
+      </Switch>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const ItemList = () => {
-    
   return (
     <div>
-        <h1>Item List</h1>
-        <ul>
-            <li>
-                <Link to={`/items/1`}>Item 1</Link>
-            </li>
-            <li>
-                <Link to={`/items/2`}>Item 2</Link>
-            </li>
-            <li>
-                <Link to={`/items/3`}>Item 3</Link>
-            </li>
-        </ul>
-    </div>
-  )
-}
+      <h1>Item List</h1>
 
-export default ItemList
+      <ul>
+        <li>
+          <Link to="/items/1">Item 1</Link>
+        </li>
+
+        <li>
+          <Link to="/items/2">Item 2</Link>
+        </li>
+
+        <li>
+          <Link to="/items/3">Item 3</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ItemList;
